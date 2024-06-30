67ca4b33c4458cc4f29fea024240fd196b4b73d9
chemguide <argentum198@gmail.com>
2024-06-30 22:41:11 +0500

    check homework please

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(8, 8, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три", "три"));
+
+        System.out.println("Task 1");
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+
+        System.out.println("\nTask 2");
+        Set<Integer> set = new TreeSet<>(nums);
+        for (Integer setMember : set) {
+            if (setMember % 2 == 0) {
+                System.out.println(setMember);
+            }
+        }
+
+        System.out.println("\nTask 3");
+        Set<String> setOfStrings = new HashSet<>(strings);
+        for (String string : setOfStrings) {
+            System.out.println(string);
+        }
+
+        System.out.println("\nTask 4");
+        Map<String, Integer> mapOfStrings = new HashMap<>();
+        for (String string : setOfStrings) {
+            int wordCounter = 0;
+            for (String s : strings) {
+                if (s.equals(string)) {
+                    mapOfStrings.put(string, ++wordCounter);
+                }
+            }
+        }
+        for (Integer wordCounter : mapOfStrings.values() ) {
+            System.out.println(wordCounter);
+        }
     }
 }
